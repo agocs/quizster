13d941bdd4204885ac03f5cc96f685d8c5db9085
christopheragocs <cagocs@backstopsolutions.com>
2014-04-27 11:11:29 -0500

    Account can now read and write from the database

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/agocs/quizster/models"
 	_ "github.com/agocs/quizster/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	models.SaveAccount(models.Account{1, "Bob", "Jones", 2014, "bjones@northwestern.edu"})
 	beego.Run()
 }
-
--- models/account_model.go
+++ models/account_model.go
@@ -1,8 +1,9 @@
 package models
 
 import (
-//"labix.org/v2/mgo"
-//"labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
 )
 
 type Account struct {
@@ -14,5 +15,44 @@ type Account struct {
 }
 
 func FindAccount(id int) Account {
-	return Account{1, "Bob", "Jones", 2018, "bjones@northwestern.edu"}
+	session, err := mgo.Dial("107.170.101.48")
+	if err != nil {
+		log.Println("Unable to connect to database.")
+		log.Panic(err.Error())
+	}
+
+	defer session.Close()
+
+	c := session.DB("quizster").C("accounts")
+
+	a := Account{}
+	err = c.Find(bson.M{"id": id}).One(&a)
+	if err != nil {
+		log.Printf("unable to find user %d", id)
+		log.Panic(err.Error())
+	}
+	log.Printf("Read Account %d from database", id)
+
+	return a
+}
+
+func SaveAccount(a Account) bool {
+	session, err := mgo.Dial("107.170.101.48")
+	if err != nil {
+		log.Println("Unable to connect to database.")
+		log.Panic(err.Error())
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("quizster").C("accounts")
+	err = c.Insert(a)
+	if err != nil {
+		log.Panic(err.Error())
+		panic(err)
+	}
+
+	log.Printf("Inserted account %d", a.Id)
+
+	return true
 }
